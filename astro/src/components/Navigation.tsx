0ecea876a0e74f8051028ed510a98b864e05f4cc
William Harrison <wharrison@horizontal.com>
2024-02-21 17:12:27 -0500

    update posts

--- astro/src/components/Navigation.tsx
+++ astro/src/components/Navigation.tsx
@@ -6,18 +6,19 @@ import {
 } from "./ui/navigation-menu";
 
 interface Props {
+  currentUrl: string;
   data: any;
 }
 
-const Navigation = ({data}: Props) => {
+const Navigation = ({data, currentUrl}: Props) => {
   return (
-    <div className="w-full flex text-9xl font-extrabold break-all tracking-tighter">
+    <div className="w-full flex text-9xl font-extrabold break-all tracking-tighter mb-10">
       <NavigationMenu className="w-full">
         <NavigationMenuList className="flex gap-8">
           {data.map((item: PostOrPage) => {
             return (
             <NavigationMenuItem>
-              <a className="text-neutral-300 hover:text-neutral-600" href={item.slug}>
+              <a className={`text-neutral-300 ${item.slug !== currentUrl && 'hover:text-neutral-600 hover:cursor-pointer'} cursor-default`} href={currentUrl !== item.slug ? item.slug : 'javascript:void(0)'}>
                 {item.title}
               </a>
             </NavigationMenuItem>
