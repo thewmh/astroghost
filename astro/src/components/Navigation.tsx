60f9bdfe3a919372f9b9b1799c92c655ceb4be05
William Harrison <wharrison@horizontal.com>
2024-02-22 16:01:05 -0500

    updates paintings

--- astro/src/components/Navigation.tsx
+++ astro/src/components/Navigation.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 const Navigation = ({data, currentUrl}: Props) => {
   return (
-    <div className="w-full flex flex-col md:text-6xl lg:text-9xl font-extrabold break-all tracking-tighter mb-10 justify-center items-center">
+    <div className="w-full flex flex-col md:text-6xl lg:text-9xl font-extrabold break-all tracking-tighter mb-4 justify-center items-center">
       <NavigationMenu className="w-full flex flex-col justify-center">
         <NavigationMenuList className="items-center justify-center flex flex-col gap-4">
           {data.map((item: PostOrPage) => {
