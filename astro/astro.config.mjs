067a747cba6f9439a26ecd0fb93628a23d150e5e
William Harrison <wharrison@horizontal.com>
2024-02-21 21:07:43 -0500

    fixes images

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,13 +2,28 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import copy from 'rollup-plugin-copy';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   build: {
     assetsPrefix: '.',
-    format: 'preserve'
   },
-  base: './'
+  base: './',
+  vite: {
+    // build: {
+    //   rollupOptions: {
+    //     plugins: [
+    //       copy({
+    //         targets: [
+    //           { src: '../content/images/**/*', dest: 'src/assets' }
+    //         ],
+    //         verbose: true,
+    //         hook: 'writeBundle'
+    //       })  
+    //     ]
+    //   }
+    // }
+  }
 });
